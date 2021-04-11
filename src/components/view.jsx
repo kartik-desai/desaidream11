cae3f2dacd9309b2af129cc3f0fa9fbf0be45c86
Kartik Desai <desaikartik99@gmail.com>
2021-04-11 13:01:29 +0530

    update 11/04.01

--- src/components/view.jsx
+++ src/components/view.jsx
@@ -8,13 +8,18 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 //let connection = new RTCMultiConnection();
 const columns = [
-
-  { field: 'name', headerName: 'Player', width: 95, sortable: false },
+  { field: 'name', headerName: 'Player', width: 93, sortable: false },
   {
     field: 'points',
     headerName: 'Points',
     type: 'number',
-    width: 80,
+    width: 90,
+  },
+  {
+    field: 'pointdifference',
+    headerName: 'Net Diff',
+    type: 'number',
+    width: 110,
   },
   {
     field: 'winloss',
@@ -23,6 +28,35 @@ const columns = [
     width: 120,
   },
 ];
+const columns2 = [
+
+  { field: 'name', headerName: 'Player', width: 93, sortable: false },
+  {
+    field: 'points',
+    headerName: 'Points',
+    type: 'number',
+    width: 90,
+  },
+  {
+    field: 'pointdifference',
+    headerName: 'Net Diff',
+    type: 'number',
+    width: 110,
+  },
+  {
+    field: 'winloss',
+    headerName: 'Win/Loss',
+    type: 'number',
+    width: 120,
+  },
+  {
+    field: 'avg',
+    headerName: 'Avg Pts',
+    type: 'number',
+    width: 110,
+  },
+];
+
 const sortModel = [
   {
     field: 'winloss',
@@ -33,7 +67,7 @@ let rows = maindata.matches[0].players;
 function Copyright() {
 return (
   <Typography variant="body2" color="error" align="center">
-    {'Last Updated : 10/04/2021, 11:11 AM '}
+    {'Last Updated : 11/04/2021, 12:39 AM '}
   </Typography>
 );
 }
@@ -46,8 +80,9 @@ class Landing extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-        match : 1,
+        match : 0,
       };
+      this.myRef = React.createRef();
     }
     componentDidMount(){
       let i = 0;
@@ -60,20 +95,38 @@ class Landing extends React.Component {
           t = maindata.matches[i].players[j].points - maindata.matches[i].players[j].winner;
           v += t;
           if (maindata.matches[i].players[j].points !== maindata.matches[i].players[j].winner)
-            maindata.matches[i].players[j].pointdifference = t;
+            maindata.matches[i].players[j].pointdifference = parseInt(t);
         }
         for(j = 0; j<maindata.matches[i].players.length;j++){
           if (maindata.matches[i].players[j].points === maindata.matches[i].players[j].winner){
-            maindata.matches[i].players[j].pointdifference = v * -1;
+            maindata.matches[i].players[j].pointdifference = parseInt(v * -1);
             j = maindata.matches[i].players.length;
           }
         }
         console.log(maindata.matches[i].players);
       }
-      rows = maindata.matches[0].players;
+      //rows = maindata.matches[0].players;
+      let players = JSON.parse(JSON.stringify(maindata.matches[0].players));
+      for(let i = 1 ;i < maindata.matches.length;i++){
+        let temp = maindata.matches[i].players;
+        for(let j= 0; j<players.length; j++){
+          players[j].points += temp[j].points;
+          if(players[j].pointdifference == null)
+            players[j].pointdifference = 0;
+          players[j].pointdifference += temp[j].pointdifference;
+          console.log(players[j].pointdifference +" "+temp[j].pointdifference);
+          players[j].winloss += temp[j].winloss;
+          players[j].played = players[j].played + 1;
+          console.log(maindata.matches.length-1);
+          if (i === maindata.matches.length-1)
+            players[j].avg = parseInt(players[j].points / players[j].played);
+        }
+      }
+      rows = players;
       this.setState({
-        match : 1,
+        match : 0,
       });
+      this.executeScroll();
     }
     setMatch = (event) =>{
       if(event.currentTarget.dataset.match > 0){
@@ -88,9 +141,15 @@ class Landing extends React.Component {
         for(let i = 1 ;i < maindata.matches.length;i++){
           let temp = maindata.matches[i].players;
           for(let j= 0; j<players.length; j++){
+            console.log(players[j]);
             players[j].points += temp[j].points;
+            if(players[j].pointdifference == null)
+              players[j].pointdifference = 0;
             players[j].pointdifference += temp[j].pointdifference;
             players[j].winloss += temp[j].winloss;
+            players[j].played = players[j].played + 1;
+            if (i === maindata.matches.length-1)
+              players[j].avg = parseInt(players[j].points / players[j].played);
           }
         }
         rows = players;
@@ -99,11 +158,12 @@ class Landing extends React.Component {
         });
       }
     }
+    executeScroll = () => this.myRef.current.scrollIntoView();
     render() {
       return (
       <div>
       <div className="header">
-        Desai
+        Desai<br/>
         <img width="370px" alt="Dream11" className="img" src="https://ttensports.com/wp-content/uploads/2019/10/Dream-11.png"/>
       </div>
       <Grid container className="root" spacing={2} style= {{placeContent: "center", marginTop: "5px"}}>
@@ -118,22 +178,28 @@ class Landing extends React.Component {
                   {maindata.matches.map(match =>{
                       return(
                         parseInt(this.state.match) === match.match ?
-                        (<ul> <Paper className="paperbtn" onClick={this.setMatch} style= {{background:"deepskyblue"}} data-match={match.match}>M{match.match}: {match.teams}</Paper> </ul> ) :
+                        (<ul> <Paper className="paperbtn" onClick={this.setMatch} style= {{background: "#e02228", color: "white"}} data-match={match.match}>M{match.match}: {match.teams}</Paper> </ul> ) :
                         (<ul> <Paper className="paperbtn" onClick={this.setMatch} data-match={match.match}>M{match.match}: {match.teams}</Paper> </ul>)
                     )
                   })}
                   {this.state.match === 0 ?
-                    (<ul> <Paper className="paperbtn" onClick={this.setMatch}style= {{background:"deepskyblue"}} data-match={0}>Overall</Paper> </ul>) :
+                    (<ul> <Paper className="paperbtn" onClick={this.setMatch}style= {{background: "#e02228", color: "white"}} data-match={0}>Overall</Paper> </ul>) :
                     (<ul> <Paper className="paperbtn" onClick={this.setMatch} data-match={0}>Overall</Paper> </ul>)
                   }
+                  <div ref = {this.myRef}/>
               </div>
               </Paper>
 
             </Grid>
             <Grid item>
               <Paper className="paper">
-              <div style={{ height: 600, width: '100%' }}>
+              <div className = "mygrid">
+                {
+                  this.state.match === 0 ?
+                  <DataGrid sortModel={sortModel} rows={rows} columns={columns2} pageSize={17} disableColumnMenu /> :
                   <DataGrid sortModel={sortModel} rows={rows} columns={columns} pageSize={17} disableColumnMenu />
+                }
+
               </div>
               </Paper>
             </Grid>
